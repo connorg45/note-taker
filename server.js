33170c6358defec7a55a0631574c6d823bb04be0
Connor <99grogan@gmail.com>
2023-08-22 11:14:18 -0700

    Set up basic code and structure

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Starts the server
+app.listen(PORT, () => {
+  console.log(`Listening on PORT: ${PORT}`)
+});
